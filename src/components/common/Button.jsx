bd241d1b7594ebc3c8f9351c1766790cdd856a51
nomanqadir-cosmoinc <nomanqadir@cosmoinc.co>
2024-12-10 12:15:02 +0500

    increse the image size and how to buy text size

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = ({ label, variant, onClick }) => {
-  const baseStyles = "px-4 py-2 text-lg font-bold rounded shadow";
+  const baseStyles = "px-6 py-2 text-lg text-[18px] font-bold font-lato text-[18px] rounded shadow";
   const variants = {
     primary: "bg-yellow-400 text-black border-2 border-blue-500 hover:bg-yellow-300",
     secondary: "bg-gray-800 text-white border-2 border-gray-800 hover:bg-gray-700",
