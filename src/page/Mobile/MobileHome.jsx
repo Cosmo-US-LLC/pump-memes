65c6624aa112a45f51002adf8b41f432aa164d6e
faheem303 <faheemrehmat360@gmail.com>
2024-12-11 14:45:26 +0500

    mobile-sections

---
+++ src/page/Mobile/MobileHome.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+import MobileHeroSec from "./MobileHeroSec";
+
+
+
+function MobileHome() {
+  return (
+    <div>
+      <MobileHeroSec />
+    </div>
+  );
+}
+
+export default MobileHome;
