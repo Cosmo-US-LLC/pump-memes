e6024dc22e134a9c4d4d5e6a87bc65794c910fa9
faheem303 <faheemrehmat360@gmail.com>
2024-12-12 19:14:28 +0500

    mobile version sections

--- src/page/Mobile/MobileFutureSection.jsx
+++ src/page/Mobile/MobileFutureSection.jsx
@@ -1,3 +1,13 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import awr1 from "../../assets/Svgs/rdawr (1).svg";
+import awr2 from "../../assets/Svgs/rdawr (2).svg";
+import rdvect1 from "../../assets/Svgs/rdvect (1).svg";
+import rdvect2 from "../../assets/Svgs/rdvect (2).svg";
 import tick from "../../assets/Svgs/tick.svg";
 import tree from "../../assets/Svgs/trees.svg";
 
@@ -36,7 +46,7 @@ function MobileFutureSection() {
                 </p>
                 <img className="mt-6 w-[200px]" src={tree} alt="tree" />
             </div>
-
+{/* 
             <div className="flex flex-col items-center gap-6 px-4">
                 <div className="bg-[#FCC105] w-full max-w-[300px] border border-[4px] border-[black] p-6">
                     <p className="text-[16px] font-[600] leading-[20px] mb-2">PHASE 1</p>
@@ -81,7 +91,121 @@ function MobileFutureSection() {
                         ))}
                     </div>
                 </div>
-            </div>
+            </div> */}
+             <div className="relative px-4">
+          {/* Swiper Slider */}
+          <Swiper
+          modules={[Navigation]}
+            spaceBetween={20}
+            slidesPerView={1}
+            loop={false}
+            className=""
+            navigation={{
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            }}
+            breakpoints={{
+              1024: {
+                slidesPerView: 3.2,
+              },
+              768: {
+                slidesPerView: 3,
+              },
+              640: {
+                slidesPerView: 1,
+              },
+              320: {
+                slidesPerView: 1,
+              },
+            }}
+          >
+            {/* Rookie Card */}
+        <SwiperSlide >
+          <div className="relative p-2"
+         >
+            <div className="bg-[#FCC105] w-full  border border-[4px] border-[black] p-[35px]">
+            <img src={rdvect1} className="absolute right-[0%] top-[-1%]" alt="" />
+
+                    <p className="text-[16px] font-[600] text-center leading-[20px] mb-2">PHASE 1</p>
+                    <p className="text-[24px] font-[600] text-center font-[Tomorrow] leading-[29px]">Idea and Concept</p>
+                    <div className="flex flex-col gap-2 mt-6">
+                        {phase1.map((item, index) => (
+                            <div key={index} className="flex flex-row items-center gap-2">
+                                <img className="w-4 h-4" src={tick} alt="tick" />
+                                <p className="text-[14px] font-[400] leading-[20px]">{item}</p>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="flex justify-end mt-4">
+                        <button className="bg-[black] text-[white] w-[100px] rounded-full h-[36px] font-[700] text-[14px]">Current</button>
+                    </div>
+                    <img src={rdvect2} className="absolute left-[0%] bottom-[-1%]" alt="" />
+                </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide >
+          <div 
+          className="relative p-2"
+         >
+             <div className="bg-[white] w-full  border border-[4px] border-[black] p-[35px]">
+            <img src={rdvect1} className="absolute right-[0%] top-[-1%]" alt="" />
+
+                    <p className="text-[16px] font-[600] text-center leading-[20px] mb-2">PHASE 2</p>
+                    <p className="text-[24px] font-[600] text-center font-[Tomorrow] leading-[29px]">Building</p>
+                    <div className="flex flex-col gap-2 mt-6">
+                        {phase2.map((item, index) => (
+                            <div key={index} className="flex flex-row items-center gap-2">
+                                {index <= 3 && <img className="w-4 h-4" src={tick} alt="tick" />}
+                                <p className={`text-[14px] font-[400] leading-[20px] ${index > 3 ? 'ml-6' : ''}`}>{item}</p>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="flex justify-end mt-4">
+                        <button className="bg-[#808080] text-[white] w-[100px] rounded-full h-[36px] font-[700] text-[14px]">Completed</button>
+                    </div>
+                <img src={rdvect2} className="absolute left-[0%] bottom-[-1%]" alt="" />
+                </div>
+
+          </div>
+        </SwiperSlide>
+        <SwiperSlide >
+          <div 
+          className="relative p-2"
+         >
+              <div className="bg-[white] w-full border border-[4px] border-[black] p-[35px]">
+            <img src={rdvect1} className="absolute right-[1%] top-[-2%]" alt="" />
+                    <p className="text-[16px] font-[600] text-center leading-[20px] mb-2">PHASE 3</p>
+                    <p className="text-[24px] font-[600] text-center font-[Tomorrow] leading-[29px]">Launch</p>
+                    <div className="flex flex-col gap-2 mt-6">
+                        {phase3.map((item, index) => (
+                            <div key={index} className="flex flex-row items-center gap-2">
+                                <p className="text-[14px] font-[400] leading-[20px]">{item}</p>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="flex justify-end mt-4">
+                        <button className="bg-[#808080] text-[white] w-[100px] rounded-full h-[36px] font-[700] text-[14px]">Completed</button>
+                    </div>
+                <img src={rdvect2} className="absolute left-[1%] bottom-[-2%]" alt="" />
+
+                </div>
+          </div>
+        </SwiperSlide>
+          </Swiper>
+          
+          {/* Navigation Arrows */}
+          <div className="!h-[35px] !w-[35px] swiper-button-prev  absolute !left-[9%] !top-[19%] -translate-y-1/2 bg-[#fff] border border-black p-2 hover:bg-white/90 z-10"   style={{
+            boxShadow: "4px 4px 0px #000",
+          }}>
+            <img src={awr1} className="" alt="Previous" />
+          </div>
+          <div className="!h-[35px] !w-[35px] swiper-button-next absolute !right-[9.5%] !top-[19%] -translate-y-1/2 bg-[#fff] border border-black p-2 hover:bg-white/90 z-10"
+          style={{
+            boxShadow: "4px 4px 0px #000",
+          }}>
+            <img src={awr2} className="" alt="Next" />
+          </div>
+        </div>
 
             <div className="self-center mt-8">
                 <button className="border border-[2px] border-[black] bg-[#FFCE00] h-[40px] w-[160px] text-[14px] font-[700]">Join Presale</button>
---
+++ src/page/Mobile/MobileRewardsSection.jsx
@@ -0,0 +1,84 @@
+import React from "react"
+import bigvector8 from "../../assets/images/rdcardicn (1).png";
+import bigvector7 from "../../assets/images/rdcardicn (2).png";
+import bigvector6 from "../../assets/images/rdcardicn (3).png";
+import bigvector5 from "../../assets/images/rdcardicn (4).png";
+import bigvector4 from "../../assets/images/rdcardicn (5).png";
+import bigvector3 from "../../assets/images/rdcardicn (6).png";
+import bigvector2 from "../../assets/images/rdcardicn (7).png";
+import bigvector1 from "../../assets/images/rdcardicn (8).png";
+
+function MobileRewardsSection() {
+  return (
+  <div className="bg-[#C8F1FF] ">
+    <div className=" bgrewardbgmob">
+    <div className="w-[90%] py-[65px] mx-auto ">
+        <div className="max-w-[1047px] w-[100%] mx-auto">
+            <div className="max-w-[689px] space-y-[30px] w-[100%] mx-auto mt-[10rem]">
+                <h3 className="text-[40px] leading-[130%] text-[#000] font-[Tomorrow] font-[700] text-center">Daily and Weekly Community Rewards</h3>
+                <p className="text-[25px] max-w-[273px] mx-auto text-center text-[#000] leading-[120.102%]">A platform designed to earn, win and have fun.</p>
+            </div>
+        </div>
+        <div className=" min-h-[100vh]  pt-[120px] pb-[120px] w-[100%] m-auto ">
+           <div className="space-y-[6rem] pb-[160px] max-w-[400px] w-[100%] ">
+           <div className="max-w-[346px] w-[100%] relative">
+                <div className="max-w-[257px]">
+                    <h4 className="text-[25px] font-[600] font-[Tomorrow] text-end leading-[120.102%] text-[#000]">Daily Airdrops</h4>
+                    <p className="text-[25px] font-[400] text-end leading-[120.102%] text-[#000]">Earn daily airdrops from coin launches.</p>
+                </div>
+                <div className="h-[150px]">
+                <div className=" flex flex-col items-end absolute !top-[-13%] right-0">
+                    <img src={bigvector2} className="mr-1" alt="" />
+                    <img src={bigvector1} alt="" />
+                </div>
+                </div>
+            </div>
+            <div className="max-w-[446px] space-x-3 w-[100%] flex items-center pl-4 relative">
+                <div className="w-[180px]">
+                <div className=" flex flex-col items-center ">
+                    <img src={bigvector4} className="max-h-[30px]" alt="" />
+                    <img src={bigvector3} alt="" />
+                </div>
+                </div>
+                <div className="max-w-[257px] pt-[20px]">
+                    <h4 className="text-[25px] font-[600] font-[Tomorrow] text-start leading-[120.102%] text-[#000]">Win Daily Prices</h4>
+                    <p className="text-[25px] font-[400] text-start leading-[120.102%] text-[#000]">Earn daily airdrops from coin launches.</p>
+                </div>
+            </div>
+           
+           </div>
+            <div className="space-y-[8rem]">
+            <div className="max-w-[340px] mx-auto w-[100%] relative">
+                <div className="h-[160px] flex justify-center">
+                <div className=" flex flex-col items-end absolute !top-[-13%]">
+                    <img src={bigvector6} className="mb-[-70px]" alt="" />
+                    <img src={bigvector5} alt="" />
+                </div>
+                </div>
+                <div className="max-w-[290px] mx-auto">
+                    <h4 className="text-[25px] font-[600] font-[Tomorrow] text-center leading-[120.102%] text-[#000]">Weekly <br />Leaderboards</h4>
+                    <p className="text-[25px] font-[400] text-center leading-[120.102%] text-[#000]">Find out who made the most profit trading on Pump Memes.</p>
+                </div>
+
+            </div>
+            <div className="max-w-[446px] space-x-3 flex  w-[100%] relative">
+                <div className="h-[220px] w-[100%] flex justify-center">
+                <div className=" flex flex-col items-start relative">
+                    <img src={bigvector8} className="max-h-[50px] absolute !top-[1%] right-[2%]" alt="" />
+                    <img src={bigvector7} alt="" />
+                </div>
+                </div>
+                <div className="w-[100%]  mx-auto">
+                    <h4 className="text-[25px] font-[600] font-[Tomorrow] text-start leading-[120.102%] text-[#000]">Anti Rug System</h4>
+                    <p className="text-[25px]  font-[400] text-start leading-[120.102%] text-[#000]">We have a system in place to prevent Rugs and Scams.</p>
+                </div>
+            </div>
+            </div>
+        </div>
+  </div>
+    </div>
+  </div>
+  );
+}
+
+export default MobileRewardsSection;
